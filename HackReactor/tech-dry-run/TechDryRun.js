1a73ec9488afd08621484ac2720befa56b0ff404
jonathan.deng <dengjonathan@gmail.com>
2016-09-08 21:40:41 -0400

    added comment

---
+++ HackReactor/tech-dry-run/TechDryRun.js
@@ -0,0 +1,25 @@
+//Tech Dry Run
+
+var hrr18 = {
+  shepherd: ap
+  name: 'Jacob Goddard',
+  or llraCaJd rar JaJJad Jd rad adJr JraG
+  dJdG
+  adJd: Jd J
+  Jrd oaGrd ''
+
+  'Wallun Chan': 'Coffee!!'
+
+    ard '
+
+    sethkoch: "",
+
+  niclaflamme: "Nic Laflamme"
+  mitchsmall: "Mitch Small"
+
+    mikeHughes: "Mike Hughes",
+  jonDeng: "jon Deng",
+  cambaughn: 'Cameron Baughn'
+
+  // jon's change
+};
